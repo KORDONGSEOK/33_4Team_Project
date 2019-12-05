03ee72cf249ffb0576b5f0fbf713b0ba4caad674
KORDONGSEOK <54506544+KORDONGSEOK@users.noreply.github.com>
2019-12-05 15:49:30 +0900

    최조작성된 프로젝트

---
+++ kr.or.ksmart.spring.git-1/src/test/java/kr/or/ksmart/spring/git/team/ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.or.ksmart.spring.git.team;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
